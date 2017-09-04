4f0ecec7aeb1bc3fa1238ca5583e076975b482e0
Astropl <30636834+Astropl@users.noreply.github.com>
2017-09-04 22:29:53 +0200

    Add files via upload

---
+++ SDAWebinar.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SDAWebinar {
+
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner (System.in);
+		//System.out.println(in);
+		System.out.println("Podaj dystans");
+		//double dystans = in.nextDouble();
+		int i = 0;
+		
+		while (i <= 10 )
+		{
+			System.out.println(i + "Podaj dystans " + i+10);
+			i++;
+			
+		}
+		//Jak Dominik pisze kod to wydaje sie takie proste i logiczne, :D a pózniej cz³owiek nie pamiêta jak System.out.println("Helo World") napisaæ :P
+		//System.out.println("Podaj zuzyte paliwo");
+		//double zuzytePaliwo = in.nextDouble();
+		
+		//double srednieSpalanie = zuzytePaliwo / (dystans/100.0);
+		//System.out.println("Srednie Spalanie to " +srednieSpalanie);
+		
+	}
+
+}
